0440ee13a946f76b2fdd767003106e8e062f5a20
Charlie Friend <charles.d.friend@gmail.com>
2016-06-20 13:50:51 -0700

    Lazy solution to environment variables problem.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send("Hello world! I'm running node.js version " + process.version + "!");
+	res.send("Hello world! I'm running node.js version " + process.version + "! :D");
 });
 
 app.listen(30052, function() {
