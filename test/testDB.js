23c562a5fd67eb1a441392bda186e7adeaf18ccd
Charlie Friend <charles.d.friend@gmail.com>
2016-06-29 17:50:00 -0700

    Add unit testing file for db functions

---
+++ test/testDB.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert      = require('assert');
+var mongoServer = require('mongodb').Server;
+var db          = require('../lib/db/MongoDB.js');
+
+describe('db', function() {
+
+	
+	
+});
