21777386c93854e62b4ac7f6f5feec33bbbde229
Charlie Friend <charles.d.friend@gmail.com>
2016-07-02 15:04:23 -0700

    Add beforeEach and afterEach to MongoDB tests.  Minor bugfixes to Mongo interface

--- test/testDB.js
+++ test/testDB.js
@@ -2,13 +2,31 @@
 
 var assert      = require('assert');
 var mongoServer = require('mongodb').Server;
+var mongoDb     = require('mongodb').Db;
 var db          = require('../lib/db/MongoDB.js');
 
 describe('db', function() {
+    
+    //start test mongoDB server
+    var server = new mongoDb('test', new mongoServer('localhost', 21707));
+    
+    var dbInst;
+    
+    beforeEach(function() {
+         dbInst = new db(null, null, 'test', 'localhost', 21707);
+         dbInst.init();
+    });
+    
+    afterEach(function() {
+        dbInst.close();
+        dbInst = null;
+    });
 
 	describe('#init()', function() {
         
-        
+        it('should be CONNECTED after initialization', function() {
+            assert.equal(db.state, 'CONNECTED');
+        });
         
     });
 	
