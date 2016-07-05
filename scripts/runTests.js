3467978519999152a29e9fc0896db67a23af81c2
Charlie Friend <charles.d.friend@gmail.com>
2016-07-05 12:40:42 -0700

    Add script for running mongo during mocha tests

---
+++ scripts/runTests.js
@@ -0,0 +1,41 @@
+#!/usr/env/node
+
+const TEST_DIR = 'test';
+
+var spawn = require('child_process').spawn;
+var Mocha = require('mocha');
+var fs    = require('fs');
+var path  = require('path');
+
+// start test db
+console.log('Starting mongo...');
+const mongoProc = spawn('mongod', ['--dbpath', './testData']);
+
+mongoProc.stdout.on('data', function(data) {
+    console.log(data.toString('utf8'));
+});
+
+mongoProc.stderr.on('data', function(data) {
+    console.log(data.toString('utf8'));
+});
+
+mongoProc.on('exit', function() {
+    console.log('Mongo exited.');
+});
+
+// get test files
+var mocha = new Mocha();
+
+fs.readdirSync(TEST_DIR).filter(function(file) {
+    return file.substr(-3) == ".js";
+}).forEach(function(file) {
+    mocha.addFile(path.join(TEST_DIR, file));
+});
+
+// run tests
+mocha.run().on('end', function() {
+      
+    //kill mongo
+    mongoProc.kill('SIGINT');
+    
+});
