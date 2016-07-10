0f9478130f901aa9e8c2b4b50699004c7d26662c
Alex <laingawbl@gmail.com>
2016-07-09 23:02:54 -0700

    added a new (tentative) version of the system UML model.

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var express = require('express');
 
 var app = express();
 
+__staticdir = 'public';
+
+app.use(express.static(__staticdir));
+
 app.get('/', function(req, res) {
 	res.send("Hello world! I'm running node.js version " + process.version + "! :D");
+});
+
+// catch requests to undefined URLs. keep this at the end!
+app.get(/.*/, function(req, res) {
+	res.sendStatus(404);
 });
 
 app.listen(30052, function() {
