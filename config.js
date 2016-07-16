3db4d4d1315661eec446563b63e54153c1c09c75
Charlie Friend <charles.d.friend@gmail.com>
2016-07-16 16:28:16 -0700

    Refactor config.js to allow for multiple modules and configurations.

--- config.js
+++ config.js
@@ -1,25 +1,40 @@
-var exports = module.exports = {};
+/*
+ * Server Configuration Module: provides data required by various modules
+ * of the software to run in a particular configuration.  For example, the
+ * module provides the location of the server's mongo database in both test
+ * and deployment environments.
+ *
+ * The {@link init()} function should be called once at the start of the
+ * program to define all the necessary information for the session.  Afterwards,
+ * config data can be found using require('config.js').data.[MODULE_NAME].
+ */
 
+var data = {};
 
+/**
+ * Defines config data for a particular session.
+ *
+ * @param cfgString {String} The configuration type to use. If this is unspecified,
+ * then the module will use the TEST configuration.
+ *
+ * Configuration types:
+ *
+ *      - TEST: For running the application on a local machine.
+ *
+ *      - DEPLOY: For running the application on the UVic engineering network.
+ */
+function init(cfgString) {
 
+    //DB Config
+    data.db.HOST = 'localhost';
+    data.db.PORT = 27017;
+    data.db.NAME = cfgString === 'DEPLOY' ? 'seng299group5' : 'test';
+    data.db.USER_COLLECTION = 'users';
+    data.db.GAME_COLLECTION = 'games';
 
-exports.host = function(){
+}
 
-        return 'localhost';
-};
-
-exports.port = function(){
-        return 27017;
-};
-
-exports.name = function(){
-        return 'test';
-};
-
-exports.user = function(){
-        return 'users';
-};
-
-exports.game = function(){
-        return 'games';
+module.exports = {
+    init : init,
+    data : data
 };
