8c80a5472378fb695d18d8ce91f33431d1d11b0c
Charlie Friend <charles.d.friend@gmail.com>
2016-07-16 15:55:09 -0700

    Move config.js to project root folder

---
+++ config.js
@@ -0,0 +1,25 @@
+var exports = module.exports = {};
+
+
+
+
+exports.host = function(){
+
+        return 'localhost';
+};
+
+exports.port = function(){
+        return 27017;
+};
+
+exports.name = function(){
+        return 'test';
+};
+
+exports.user = function(){
+        return 'users';
+};
+
+exports.game = function(){
+        return 'games';
+};
