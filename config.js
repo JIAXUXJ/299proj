9a48383d68bccd01d121e620859eaa9f5d53114a
Charlie Friend <charles.d.friend@gmail.com>
2016-07-16 17:25:56 -0700

    Adapt MongoDB to work with config.js. More refactoring on config.js.

--- config.js
+++ config.js
@@ -4,37 +4,24 @@
  * module provides the location of the server's mongo database in both test
  * and deployment environments.
  *
- * The {@link init()} function should be called once at the start of the
- * program to define all the necessary information for the session.  Afterwards,
- * config data can be found using require('config.js').data.[MODULE_NAME].
+ * Configuration types:
+ *
+ *     - TEST: For running the application on a local machine.
+ *
+ *     - DEPLOY: For running the application on the UVic engineering network.
  */
+
+const CFG_NAME = 'TEST';
 
 var data = {};
 
-/**
- * Defines config data for a particular session.
- *
- * @param cfgString {String} The configuration type to use. If this is unspecified,
- * then the module will use the TEST configuration.
- *
- * Configuration types:
- *
- *      - TEST: For running the application on a local machine.
- *
- *      - DEPLOY: For running the application on the UVic engineering network.
- */
-function init(cfgString) {
+//DB Config
+data.db = {};
+data.db.HOST = 'localhost';
+data.db.PORT = 27017;
+data.db.NAME = CFG_NAME === 'DEPLOY' ? 'seng299group5' : 'test';
+data.db.USER_COLLECTION = 'users';
+data.db.GAME_COLLECTION = 'games';
 
-    //DB Config
-    data.db.HOST = 'localhost';
-    data.db.PORT = 27017;
-    data.db.NAME = cfgString === 'DEPLOY' ? 'seng299group5' : 'test';
-    data.db.USER_COLLECTION = 'users';
-    data.db.GAME_COLLECTION = 'games';
 
-}
-
-module.exports = {
-    init : init,
-    data : data
-};
+module.exports = data;
