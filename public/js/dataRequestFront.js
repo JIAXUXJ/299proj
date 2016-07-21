97cd9e953a574cdaa7516c3bf4113fcfd042a0ba
alxlax <alxreb@gmail.com>
2016-07-21 15:29:59 -0700

    updating using Jia's Public folder

--- public/js/dataRequestFront.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Joye on 2016-07-19.
- */
-// function BoardConvas() {
-//     var getxhr = new XMLHttpRequest();
-// }
-// var boardConvas = new BoardConvas();
-// boardConvas.init();
--- public/js/style.js
+++ public/js/style.js
@@ -4,38 +4,21 @@
 var Appearance = {
     init: function () {
         this.guideHighlighter();
-        this.showTeamMember();
     },
     guideHighlighter: function () {
-        $('.top-nav li:not(".here")').hover(function(){
+        $('.top-nav li:not("here")').hover(function(){
             $(this).addClass('nav-on');
         },function () {
             $(this).removeClass('nav-on');
         });
         $('#userSetting').hover(function () {
-            console.log(typeof $('.remainder'))
             $('.remainder').show();
         }, function () {
             $('.remainder').hide();
         });
-        $('.try-play').hover(function () {
-            $('.rmd').show();
-        }, function () {
-            $('.rmd').hide();
-        });
-        $('.selection-nav li:not(".here")').hover(function () {
-            $(this).addClass('selection-nav-on');
-        },function () {
-            $(this).removeClass('selection-nav-on');
-        });
-    },
-    showTeamMember: function () {
-        $('.team-img').hover(function () {
-            // console.log(typeof $(this)[0].childNodes[1])
-            $(this).find('.individual').hide();
-        },function () {
-            $('.individual').show();
-        });
+        $('.selection-nav').hover(function () {
+            
+        })
     }
 
 };
